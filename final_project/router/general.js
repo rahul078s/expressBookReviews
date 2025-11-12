6c579ea6db3544983e47315aee17b686d05ed8a9
Rahul Sandhu <rahulsandhu527@gmail.com>
2025-11-12 12:17:44 -0500

    Added route handler to get book using specifief properties

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,28 +12,63 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(200).json({
+        message: "List of all available books",
+        books: books
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Extract isbn from req
+    const isbn = req.params.isbn;
+    // Check if isbn provided
+    if (books[isbn]) {
+        return res.status(200).json(books[isbn]);
+    } else {
+        return res.status(404).json({message: "Book not found"});
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    // Extract author from req
+    const author = req.params.author;
+    let booksByAuthor = [];
+    for (let key in books) {
+        if (books[key].author.toLowerCase().trim() === author.toLowerCase().trim()) {
+            booksByAuthor.push(books[key]);
+        }
+        // If no book found
+        if (booksByAuthor.length === 0) {
+            return res.status(404).json({message: "No books with the specified author"});
+        }
+        // Return all books found
+        return res.status(200).json(booksByAuthor);
+    }
+}); 
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', (req, res) => {
+    const title = req.params.title;     // Extract the title from URL
+    const booksByTitle = [];            // Array to store matches
+  
+    // Loop through all books in the database
+    for (let key in books) {
+      if (books[key].title.toLowerCase().trim() === title.toLowerCase().trim()) {
+        booksByTitle.push(books[key]);
+      }
+    }
+  
+    // If no books found, send 404
+    if (booksByTitle.length === 0) {
+      return res.status(404).json({ message: "No books found with that title" });
+    }
+  
+    // Send all matched books
+    return res.status(200).json(booksByTitle);
 });
-
+  
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
