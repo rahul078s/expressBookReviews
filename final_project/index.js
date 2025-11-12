191c455032f7bfe7f4af518decdb70faf2963a58
Rahul Sandhu <rahulsandhu527@gmail.com>
2025-11-12 07:59:06 -0500

    Updated authentication middleware code

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,23 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    // Check if the session has an auhtorization object
+    if (req.session.authorization) {
+        // Extract the token from session
+        let token = req.session.authorization["accessToken"];
+
+        // Verify the JWT Token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user; // attach decoded token data to req
+                next(); // allow access to next route
+            } else {
+                return res.status(403).json({message: "User not authenticated"});
+            }
+        });
+    } else {
+        res.status(403).json({message: "User not logged in"});
+    }
 });
  
 const PORT =5000;
